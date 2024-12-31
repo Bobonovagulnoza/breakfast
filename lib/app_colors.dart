3ab240ffc6b937f507c35c7dbd6b45a121c21a88
Bobonovagulnoza <bobonovagulnoza68@gmail.com>
2024-12-31 12:13:59 +0500

    first commit

---
+++ lib/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color BackgroundColor = Color(0xFF221D22);
+  static const Color RedPinkMain = Color(0xFFFD5D69);
+  static const Color Pink = Color(0xFFFFC6C9);
+  static const Color TextsColor = Color(0xFF3E2823);
+  static const Color RatingColor = Color(0xFFEC888D);
+
+  static var mianTextColor;
+}
